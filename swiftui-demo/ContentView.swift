5a3ee9d9b159eae9aaa72c30f5bf59f5d9b2d9e4
ttzztztz <ttzztztz@outlook.com>
2020-02-12 21:24:08 +0800

    Initial Commit

---
+++ swiftui-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-demo
+//
+//  Created by 杨子越 on 2020/2/12.
+//  Copyright © 2020 杨子越. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
